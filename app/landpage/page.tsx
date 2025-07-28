f6fab638c47425b5060bd2325fd263854aadeda4
Grian Gajila <griangajila2@gmail.com>
2025-07-28 18:13:57 +0800

    feat: add landpage

---
+++ app/landpage/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+
+const Landpage = () => {
+	return (
+		<div className="flex items-center justify-center h-screen w-full bg-[url('/cover.png')] bg-cover bg-no-repeat bg-fixed bg-center">
+			<div className="flex flex-col text-center gap-10">
+				<div className="flex flex-col justify-center items-center">
+					<h1 className="text-slate-900 text-4xl font-bold">
+						Hi, Welcome to Rose BelEase
+					</h1>
+					<p className="text-slate-800 text-2xl text-center font-medium text-wrap max-w-[450px]">
+						Your self-care, your way. Book, manage, and glow-all in one app.
+					</p>
+				</div>
+				<div className="flex gap-14">
+					<Button className="w-[250px] border bg-transparent hover:bg-slate-100 hover:cursor-pointer border-slate-900">
+						Create Account
+					</Button>
+					<Button className="bg-slate-900 w-[250px] hover:bg-slate-700 hover:cursor-pointer text-white">
+						Log in
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Landpage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
 export default function Home() {
+	const router = useRouter();
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			router.push("/landpage");
+		}, 2000);
+	});
+
 	return (
-		<div className="bg-[url('/cover.png')] bg-no-repeat bg-cover bg-bottom h-screen w-full"></div>
+		<div className="bg-[url('/startup.png')] bg-no-repeat bg-cover bg-bottom h-screen w-full"></div>
 	);
 }
