d04314b6963fa02c08f8d8e1cc5d6bee1a8698b1
Grian Gajila <griangajila2@gmail.com>
2025-07-29 01:43:45 +0800

    wip

--- constant/index.ts
+++ constant/index.ts
@@ -1,6 +1,6 @@
 import Google from "@/assets/icon/google.png";
-import signInLogo from "@/assets/images/signInLogo.png";
-import signUpLogo from "@/assets/images/signUpLogo.png";
 import RoseBelEaseLogo from "@/assets/logo/RoseBelEaseLogo.png";
+import signInLogo from "../assets/images/signInLogo.png";
+import signUpLogo from "../assets/images/signUpLogo.png";
 
 export { Google, RoseBelEaseLogo, signInLogo, signUpLogo };
